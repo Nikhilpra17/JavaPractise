241fe4c00d4f61fddf7bdad76732b4fee4ab1e13
Nikhil Pratap Singh <pratapnikhil106@gmail.com>
2022-03-20 16:03:53 +0530

    String practise

---
+++ src/com/company/Stringpract.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Stringpract {
+    public static void main(String[] args) {
+    String a = "Nikhil";
+    String lip = a.toLowerCase();
+        System.out.println(a.substring(1,4));
+
+    }
+}
