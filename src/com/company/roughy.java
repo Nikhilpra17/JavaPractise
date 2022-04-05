88c626db527008f86508d0d9efb89d26cd0c0e47
Nikhil Pratap Singh <pratapnikhil106@gmail.com>
2022-04-05 12:45:43 +0530

    2D Array

--- src/com/company/roughy.java
+++ src/com/company/roughy.java
@@ -4,15 +4,12 @@ import java.text.DecimalFormat;
 
  public class roughy {
      public static void main(String[] args) {
-
-         for (int i = 1; i <= 3; i++) {
-             for (int g = 1; g <= 3 - i; g++) {
-                 System.out.print("$");
-             }
-             for (int d = 1; d <= i; d++) {
-                 System.out.print("*");
-             }
-             System.out.print("\n");
+         // args = {"Adi", "Nikhil", "Something"}
+//         for (String argy: args) {
+//             System.out.println(argy);
+//         }
+         for(int i=0; i < args.length; i++){
+             System.out.println(args[i]);
          }
      }
  }
